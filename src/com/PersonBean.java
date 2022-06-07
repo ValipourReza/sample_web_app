b93ffd09214b83bd7d04f5d2ea1f2cee6d97b46e
ValipourReza <107016452+ValipourReza@users.noreply.github.com>
2022-06-06 21:47:06 -0600

    اضافه کردن سورس

---
+++ src/com/PersonBean.java
@@ -0,0 +1,31 @@
+package com;
+
+public class PersonBean {
+    private String firstName = "";
+    private String lastName = "";
+    private String fatherName = "";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+}
